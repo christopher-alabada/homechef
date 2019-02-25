b9ddeb8a8bc1a0ae8d91291113a4712662672036
Christopher Alabada <christopher.alabada@gmail.com>
2019-02-25 17:46:59 +0900

    Added user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chefs, only: [:index, :show, :create, :new]
+
+  namespace :foodie do
+    resources :bookings, only: [:index, :create]
+  end
+
+  namespace :chef do
+    resources :bookings, only: [:index, :update]
+  end
 end
