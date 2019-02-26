c1622b30e4fd4ffb6a9ee1c2ddfa366fd65345c4
Kumi Takenaka <kumireliy@gmail.com>
2019-02-26 18:11:11 +0900

    chef_index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_051919) do
+ActiveRecord::Schema.define(version: 2019_02_26_015038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,15 +24,6 @@ ActiveRecord::Schema.define(version: 2019_02_26_051919) do
     t.datetime "updated_at", null: false
     t.index ["chef_id"], name: "index_bookings_on_chef_id"
     t.index ["foodie_id"], name: "index_bookings_on_foodie_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "stars"
-    t.text "content"
-    t.integer "foodie_id"
-    t.integer "chef_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
