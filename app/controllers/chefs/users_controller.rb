b2f29705376884f6e74bd4a202960f4c0503e90d
Christopher Alabada <christopher.alabada@gmail.com>
2019-02-27 11:41:58 +0900

    Configured a basic use of pundit

--- app/controllers/chefs/users_controller.rb
+++ app/controllers/chefs/users_controller.rb
@@ -1,10 +1,16 @@
 class Chefs::UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_after_action :verify_authorized, only: :show
+
   def index
-    @chefs = User.where(role: 'chef')
+    @chefs = policy_scope(User)
+    # @chefs = User.where(role: 'chef')
+    #
   end
 
   def show
     @chef = User.find(params[:id])
     @chef_full_name = "#{@chef.first_name} #{@chef.last_name}"
+    # skip_authorization
   end
 end
---
+++ app/policies/page_policy.rb
@@ -0,0 +1,5 @@
+class PagePolicy < ApplicationPolicy
+  def home?
+    true
+  end
+end
