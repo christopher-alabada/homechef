9c79328e46f1de0a8231c61243521e3febc001fe
Christopher Alabada <christopher.alabada@gmail.com>
2019-02-28 23:21:19 +0900

    Create /kitchen/foodie with edit profile

--- app/controllers/kitchen/users_controller.rb
+++ app/controllers/kitchen/users_controller.rb
@@ -1,10 +1,33 @@
 class Kitchen::UsersController < ApplicationController
+  before_action :set_foodie, only: [:show, :edit, :update]
+
   def show
-    @foodie = User.find(current_user[:id])
+    authorize @foodie
+    @bookings = Booking.where(foodie_id: current_user[:id]).order(date: :desc)
+  end
+
+  def edit
     authorize @foodie
   end
 
-  def udpate
-    raise
+  def update
+    @foodie.update(kitchen_user_params)
+    authorize @foodie
+
+    if @foodie.save
+      redirect_to kitchen_foodie_path
+    else
+      redirect_to kitchen_edit_foodie_path(@foodie)
+    end
+  end
+
+  private
+
+  def set_foodie
+    @foodie = User.find(current_user[:id])
+  end
+
+  def kitchen_user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :photo)
   end
 end
