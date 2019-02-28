b10db0d0829cdbe80b0f683a3468f1da12f8189f
Yuki Mori <masked04@gmail.com>
2019-02-28 18:02:07 +0900

    created new chef function

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -13,12 +13,14 @@ class ChefsController < ApplicationController
   end
 
   def new
-    @chefs = Chef.new
-    authorize(@chefs)
+    @chef = Chef.new
+    authorize(@chef)
   end
 
   def create
+    # raise
     @chef = Chef.new(permit_create_chef)
+    @chef.user = current_user
     if @chef.save
       redirect_to chef_path(@chef)
     else
@@ -29,6 +31,9 @@ class ChefsController < ApplicationController
   private
 
   def permit_create_chef
-    params.require(:chef).permit(:tagline, :content, :banner_photo, :card_photo, :user_photo)
+    params.require(:chef).permit(:tagline, :content,
+      :banner_photo, :banner_photo_cache,
+      :card_photo, :card_photo_cache,
+      :user_photo, :user_photo_cache)
   end
 end
