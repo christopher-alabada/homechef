c8f183c22884de7154a063bb73ee2eb10981c590
Kumi Takenaka <kumireliy@gmail.com>
2019-02-28 18:19:08 +0900

    prevent booking if date is blank

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
 
   belongs_to :foodie, class_name: 'User', foreign_key: 'foodie_id'
   belongs_to :chef, class_name: 'Chef', foreign_key: 'chef_id'
+
+  validates :date, presence: true
 end
