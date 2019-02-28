d635fa17801f824b2dab4f103bcf4b3b48247dcf
Kumi Takenaka <kumireliy@gmail.com>
2019-02-28 10:35:54 +0900

    added booking on show page

--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,6 @@ ActiveRecord::Schema.define(version: 2019_02_26_143052) do
     t.index ["foodie_id"], name: "index_bookings_on_foodie_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "stars"
-    t.text "content"
-    t.integer "foodie_id"
-    t.integer "chef_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
