de1cdedaabdeb7e52adc87d41e7bdc1d6124d5e2
Yuki Mori <masked04@gmail.com>
2019-03-01 12:34:02 +0900

    become a chef and show page

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -32,7 +32,7 @@ class ChefsController < ApplicationController
   private
 
   def permit_create_chef
-    params.require(:chef).permit(:tagline, :content,
+    params.require(:chef).permit(:tagline, :content, :units,
       :banner_photo, :banner_photo_cache,
       :card_photo, :card_photo_cache)
   end
