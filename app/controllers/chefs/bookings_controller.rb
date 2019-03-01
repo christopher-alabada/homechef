1b2912882797e1cdd3c0e56825b62b8b52036987
Kumi Takenaka <kumireliy@gmail.com>
2019-03-01 11:28:09 +0900

    fixed booking root path

--- app/controllers/chefs/bookings_controller.rb
+++ app/controllers/chefs/bookings_controller.rb
@@ -1,27 +1,27 @@
-class Chefs::BookingsController < ApplicationController
-  def new
-    @chef = Chef.find(params[:id])
-    @booking = Booking.new
-    authorize @booking
-  end
+# class Chefs::BookingsController < ApplicationController
+#   def new
+#     @chef = Chef.find(params[:id])
+#     @booking = Booking.new
+#     authorize @booking
+#   end
 
-   def create
-    @chef = Chef.find(params[:chef_id])
-    @booking = Booking.new(booking_params)
-    @booking.foodie = User.find(current_user.id)
-    @booking.chef = Chef.find(params[:chef_id])
-    authorize @booking
+#    def create
+#     @chef = Chef.find(params[:chef_id])
+#     @booking = Booking.new(booking_params)
+#     @booking.foodie = User.find(current_user.id)
+#     @booking.chef = Chef.find(params[:chef_id])
+#     authorize @booking
 
-    if @booking.save
-      redirect_to kitchen_bookings_path
-    else
-      redirect_to chef_path(@chef)
-    end
-  end
+#     if @booking.save
+#       redirect_to kitchen_foodie_bookings_path
+#     else
+#       redirect_to chef_path(@chef)
+#     end
+#   end
 
-  private
+#   private
 
-  def booking_params
-    params.require(:booking).permit(:date)
-  end
-end
+#   def booking_params
+#     params.require(:booking).permit(:date)
+#   end
+# end
